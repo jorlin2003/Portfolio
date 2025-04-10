f82d7362cc6a79bae066b7386f37ef7b0c193009
jolin roy <jorlinroy@gmail.com>
2025-04-10 00:19:13 -0400

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 /*
 // Student Name: Jorlin Roy
 // Student ID: 301240499
- Date: 7 OCT 2024*/
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import MainRouter from '../MainRouter';
-import Contact from './contact'
-const App = () => {
- return (
- <Router>
- 
- <MainRouter />
+// Date: 7 OCT 2024
+*/
 
- </Router>
- );
+import React from 'react';
+import { HashRouter as Router } from 'react-router-dom';
+import MainRouter from '../MainRouter'; // Assuming your main routing logic is here
+
+const App = () => {
+  return (
+    <Router basename="/Portfolio"> {/* Make sure to use basename with your repo name */}
+      <MainRouter />
+    </Router>
+  );
 };
+
 export default App;
