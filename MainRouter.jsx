b6328f0b806a9316e553e0f55f25e464373a42e2
jolin roy <jorlinroy@gmail.com>
2025-04-11 03:33:52 -0400

    commit'

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import About from './src/about'
-import Contact from './src/contact'
-import Education from './src/education'
-import Project from './src/project'
-import Layout from './components/Layout'
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from "./components/Home";  // Adjusted path for Home component
+import About from './src/about';           // Assuming About, Education, etc., are in src
+import Education from './src/education';
+import Project from './src/project';  
+import Contact from './src/contact';  
+import Layout from './components/Layout';  // Assuming Layout is also in src or components
 
 const MainRouter = () => {
- return (<div>
- <Layout/>
- <Routes>
- 
- <Route exact path="/" element={<Home />} />
- <Route exact path="/about" element={<About />} />
- <Route exact path="/education" element={<Education />} />
- <Route exact path="/project" element={<Project />} />
- <Route exact path="/contact" element={<Contact />} />
- 
- </Routes>
- </div>
-)
-}
-export default MainRouter
+  return (
+    <>
+      <Layout />  {/* Render the layout with the navigation bar */}
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
+
+export default MainRouter;
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 /*
 // Student Name: Jorlin Roy
 // Student ID: 301240499
- Date: 7 OCT 2024*/
+// Date: 7 OCT 2024
+*/
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import MainRouter from '../MainRouter';
-import Contact from './contact'
-const App = () => {
- return (
- <Router>
- 
- <MainRouter />
 
- </Router>
- );
+const App = () => {
+  return (
+    <Router basename="/Portfolio">
+      <MainRouter />
+    </Router>
+  );
 };
+
 export default App;
