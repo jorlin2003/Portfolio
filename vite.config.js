af2c17f50d1bf1340183709c9925537a25efd1a2
jolin roy <jorlinroy@gmail.com>
2025-04-11 02:57:03 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio/", 
+  base: '/Portfolio/', // 👈 Set the correct subpath here
   plugins: [react()],
-})
+});
